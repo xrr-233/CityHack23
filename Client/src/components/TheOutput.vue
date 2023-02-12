c6cb7660cb82fcdbe1fcac69abd37ef3a1b25d8d
xrr-233 <ruixu33-c@my.cityu.edu.hk>
2023-02-12 13:48:05 +0800

    almost finish

--- Client/src/components/TheOutput.vue
+++ Client/src/components/TheOutput.vue
@@ -20,167 +20,167 @@ defineProps({
           </tr>
           <tr>
             <td>Start Date: </td>
-            <td class="table-data">{{ status.data.Strategy.start }}</td>
+            <td class="table-data">{{ status.data[Object.keys(status.data)[1]].start }}</td>
           </tr>
           <tr>
             <td>End Date: </td>
-            <td class="table-data">{{ status.data.Strategy.end }}</td>
+            <td class="table-data">{{ status.data[Object.keys(status.data)[1]].end }}</td>
           </tr>
           <tr>
             <td>Total Return: </td>
-            <td class="table-data">{{ status.data.Strategy.total_return }}</td>
+            <td class="table-data">{{ status.data[Object.keys(status.data)[1]].total_return }}</td>
           </tr>
           <tr>
             <td>CAGR (Compound annual growth rate): </td>
-            <td class="table-data">{{ status.data.Strategy.cagr }}</td>
+            <td class="table-data">{{ status.data[Object.keys(status.data)[1]].cagr }}</td>
           </tr>
           <tr>
             <th colspan="2" class="text-center">Risk-adjusted Ratios</th>
           </tr>
           <tr>
             <td>Max Drawdown: </td>
-            <td class="table-data">{{ status.data.Strategy.max_drawdown }}</td>
+            <td class="table-data">{{ status.data[Object.keys(status.data)[1]].max_drawdown }}</td>
           </tr>
           <tr>
             <td>Calmar ratio: </td>
-            <td class="table-data">{{ status.data.Strategy.calmar }}</td>
+            <td class="table-data">{{ status.data[Object.keys(status.data)[1]].calmar }}</td>
           </tr>
           <tr>
             <th colspan="2" class="text-center">Interest Rate</th>
           </tr>
           <tr>
             <td>MTD (Month to date): </td>
-            <td class="table-data">{{ status.data.Strategy.mtd }}</td>
+            <td class="table-data">{{ status.data[Object.keys(status.data)[1]].mtd }}</td>
           </tr>
           <tr>
             <td>Three months: </td>
-            <td class="table-data">{{ status.data.Strategy.three_month }}</td>
+            <td class="table-data">{{ status.data[Object.keys(status.data)[1]].three_month }}</td>
           </tr>
           <tr>
             <td>Six months: </td>
-            <td class="table-data">{{ status.data.Strategy.six_month }}</td>
+            <td class="table-data">{{ status.data[Object.keys(status.data)[1]].six_month }}</td>
           </tr>
           <tr>
             <td>YTD (Year to date): </td>
-            <td class="table-data">{{ status.data.Strategy.ytd }}</td>
+            <td class="table-data">{{ status.data[Object.keys(status.data)[1]].ytd }}</td>
           </tr>
           <tr>
             <td>One year: </td>
-            <td class="table-data">{{ status.data.Strategy.one_year }}</td>
+            <td class="table-data">{{ status.data[Object.keys(status.data)[1]].one_year }}</td>
           </tr>
           <tr>
             <td>Three years: </td>
-            <td class="table-data">{{ status.data.Strategy.three_year }}</td>
+            <td class="table-data">{{ status.data[Object.keys(status.data)[1]].three_year }}</td>
           </tr>
           <tr>
             <td>Five years: </td>
-            <td class="table-data">{{ status.data.Strategy.five_year }}</td>
+            <td class="table-data">{{ status.data[Object.keys(status.data)[1]].five_year }}</td>
           </tr>
           <tr>
             <td>Ten years: </td>
-            <td class="table-data">{{ status.data.Strategy.ten_year }}</td>
+            <td class="table-data">{{ status.data[Object.keys(status.data)[1]].ten_year }}</td>
           </tr>
           <tr>
             <th colspan="2" class="text-center">Measures</th>
           </tr>
           <tr>
             <td>Daily sharpe: </td>
-            <td class="table-data">{{ status.data.Strategy.daily_sharpe }}</td>
+            <td class="table-data">{{ status.data[Object.keys(status.data)[1]].daily_sharpe }}</td>
           </tr>
           <tr>
             <td>Daily sortino: </td>
-            <td class="table-data">{{ status.data.Strategy.daily_sortino }}</td>
+            <td class="table-data">{{ status.data[Object.keys(status.data)[1]].daily_sortino }}</td>
           </tr>
           <tr>
             <td>Daily mean: </td>
-            <td class="table-data">{{ status.data.Strategy.daily_mean }}</td>
+            <td class="table-data">{{ status.data[Object.keys(status.data)[1]].daily_mean }}</td>
           </tr>
           <tr>
             <td>Daily vol: </td>
-            <td class="table-data">{{ status.data.Strategy.daily_vol }}</td>
+            <td class="table-data">{{ status.data[Object.keys(status.data)[1]].daily_vol }}</td>
           </tr>
           <tr>
             <td>Daily skew: </td>
-            <td class="table-data">{{ status.data.Strategy.daily_skew }}</td>
+            <td class="table-data">{{ status.data[Object.keys(status.data)[1]].daily_skew }}</td>
           </tr>
           <tr>
             <td>Daily kurt: </td>
-            <td class="table-data">{{ status.data.Strategy.daily_kurt }}</td>
+            <td class="table-data">{{ status.data[Object.keys(status.data)[1]].daily_kurt }}</td>
           </tr>
           <tr>
             <td>Monthly sharpe: </td>
-            <td class="table-data">{{ status.data.Strategy.monthly_sharpe }}</td>
+            <td class="table-data">{{ status.data[Object.keys(status.data)[1]].monthly_sharpe }}</td>
           </tr>
           <tr>
             <td>Monthly sortino: </td>
-            <td class="table-data">{{ status.data.Strategy.monthly_sortino }}</td>
+            <td class="table-data">{{ status.data[Object.keys(status.data)[1]].monthly_sortino }}</td>
           </tr>
           <tr>
             <td>Monthly mean: </td>
-            <td class="table-data">{{ status.data.Strategy.monthly_mean }}</td>
+            <td class="table-data">{{ status.data[Object.keys(status.data)[1]].monthly_mean }}</td>
           </tr>
           <tr>
             <td>Monthly vol: </td>
-            <td class="table-data">{{ status.data.Strategy.monthly_vol }}</td>
+            <td class="table-data">{{ status.data[Object.keys(status.data)[1]].monthly_vol }}</td>
           </tr>
           <tr>
             <td>Monthly skew: </td>
-            <td class="table-data">{{ status.data.Strategy.monthly_skew }}</td>
+            <td class="table-data">{{ status.data[Object.keys(status.data)[1]].monthly_skew }}</td>
           </tr>
           <tr>
             <td>Monthly kurt: </td>
-            <td class="table-data">{{ status.data.Strategy.monthly_kurt }}</td>
+            <td class="table-data">{{ status.data[Object.keys(status.data)[1]].monthly_kurt }}</td>
           </tr>
           <tr>
             <td>Yearly sharpe: </td>
-            <td class="table-data">{{ status.data.Strategy.yearly_sharpe }}</td>
+            <td class="table-data">{{ status.data[Object.keys(status.data)[1]].yearly_sharpe }}</td>
           </tr>
           <tr>
             <td>Yearly sortino: </td>
-            <td class="table-data">{{ status.data.Strategy.yearly_sortino }}</td>
+            <td class="table-data">{{ status.data[Object.keys(status.data)[1]].yearly_sortino }}</td>
           </tr>
           <tr>
             <td>Yearly mean: </td>
-            <td class="table-data">{{ status.data.Strategy.yearly_mean }}</td>
+            <td class="table-data">{{ status.data[Object.keys(status.data)[1]].yearly_mean }}</td>
           </tr>
           <tr>
             <td>Yearly vol: </td>
-            <td class="table-data">{{ status.data.Strategy.yearly_vol }}</td>
+            <td class="table-data">{{ status.data[Object.keys(status.data)[1]].yearly_vol }}</td>
           </tr>
           <tr>
             <td>Yearly skew: </td>
-            <td class="table-data">{{ status.data.Strategy.yearly_skew }}</td>
+            <td class="table-data">{{ status.data[Object.keys(status.data)[1]].yearly_skew }}</td>
           </tr>
           <tr>
             <td>Yearly kurt: </td>
-            <td class="table-data">{{ status.data.Strategy.yearly_kurt }}</td>
+            <td class="table-data">{{ status.data[Object.keys(status.data)[1]].yearly_kurt }}</td>
           </tr>
           <tr>
             <th colspan="2" class="text-center">Others</th>
           </tr>
           <tr>
             <td>Best day: </td>
-            <td class="table-data">{{ status.data.Strategy.best_day }}</td>
+            <td class="table-data">{{ status.data[Object.keys(status.data)[1]].best_day }}</td>
           </tr>
           <tr>
             <td>Best month: </td>
-            <td class="table-data">{{ status.data.Strategy.best_month }}</td>
+            <td class="table-data">{{ status.data[Object.keys(status.data)[1]].best_month }}</td>
           </tr>
           <tr>
             <td>Best year: </td>
-            <td class="table-data">{{ status.data.Strategy.best_year }}</td>
+            <td class="table-data">{{ status.data[Object.keys(status.data)[1]].best_year }}</td>
           </tr>
           <tr>
             <td>Worst day: </td>
-            <td class="table-data">{{ status.data.Strategy.worst_day }}</td>
+            <td class="table-data">{{ status.data[Object.keys(status.data)[1]].worst_day }}</td>
           </tr>
           <tr>
             <td>Worst month: </td>
-            <td class="table-data">{{ status.data.Strategy.worst_month }}</td>
+            <td class="table-data">{{ status.data[Object.keys(status.data)[1]].worst_month }}</td>
           </tr>
           <tr>
             <td>Worst year: </td>
-            <td class="table-data">{{ status.data.Strategy.worst_year }}</td>
+            <td class="table-data">{{ status.data[Object.keys(status.data)[1]].worst_year }}</td>
           </tr>
         </table>
       </div>
